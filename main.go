4b3cca89affa9ddc04198561ac369111144c063a
neiyar <subhani17786@gmail.com>
2025-06-27 11:37:37 +0530

    update

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Print("hello world!");
+
+     app:=fiber.New()
+	 app.Get("/abc",func(c*fiber.Ctx)error  {
+		return c.SendString("hello world!")
+	 })
+	 app.Listen(":3000")
+}
