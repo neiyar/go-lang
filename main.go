0b9fb993a82fe1a4b8adf2ff56c3e6561d1c2c20
neiyar <subhani17786@gmail.com>
2025-06-30 11:54:48 +0530

    change

--- main.go
+++ main.go
@@ -7,12 +7,37 @@ import (
 )
 
 func main() {
-	fmt.Print("hello world neiyar! ");
 
+	fmt.Println("Hello, World!")
+	app := fiber.New()
+	app.Get("/abc", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World! 1234")
+	})
+	app.Post("/sum", AddOperation)
+	app.Listen(":3000")
 
-     app:=fiber.New()
-	 app.Get("/abc",func(c*fiber.Ctx)error  {
-		return c.SendString("hello world Neiyar Subhani. how are you neiyar!")
-	 })
-	 app.Listen(":3000")
 }
+
+func AddOperation(c *fiber.Ctx) error {
+	var request AddOperationRequest
+	if err := c.BodyParser(&request); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid request body",
+		})
+	}
+
+	result := request.Num1 + request.Num2
+	response := AddOperationResponse{Result: result}
+
+	return c.JSON(response) 
+}
+
+type AddOperationRequest struct {
+	Num1 int  `json:"num1"`
+	Num2 int  `json:"num2"`
+}
+
+type AddOperationResponse struct {
+	Result int `json:"result"`
+}
+	
